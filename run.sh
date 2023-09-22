42051c805f8c7eee3f5046c26d70d3f8762f37a6
MasakiFujiwara <fmasaki2020@gmail.com>
2023-09-23 02:13:08 +0900

    update for tokuron

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "docker container run --network host --gpus all -it --name my-humble -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged --env="XAUTHORITY=$XAUTH" -v "$XAUTH:$XAUTH" --env="QT_X11_NO_MITSHM=1" --ipc=host masakifujiwara1/ros2:humble"
+eval "docker container run --network host --gpus all -it --name my-tokuron -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged --env="XAUTHORITY=$XAUTH" -v "$XAUTH:$XAUTH" --env="QT_X11_NO_MITSHM=1" --ipc=host masakifujiwara1/ros2:humble"
