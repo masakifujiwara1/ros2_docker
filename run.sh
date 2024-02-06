1c8c58b461490640422254fba9e102ca4d687e9a
MasakiFujiwara <fmasaki2020@gmail.com>
2024-02-07 06:53:18 +0900

    fix bind mount

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "docker container run --network host -it --name my-humble-orne-box3-sim -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged --env="XAUTHORITY=$XAUTH" -v "$XAUTH:$XAUTH" --env="QT_X11_NO_MITSHM=1" --ipc=host masakifujiwara1/ros2:humble-orne-box3-sim"
+eval "docker container run --network host -it --name my-humble-orne-box3-sim -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged -v /dev:/dev --env="XAUTHORITY=$XAUTH" -v "$XAUTH:$XAUTH" --env="QT_X11_NO_MITSHM=1" --ipc=host masakifujiwara1/ros2:humble-orne-box3-sim"
